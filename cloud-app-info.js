e78535ae636096be276da08ca82553c658fcb91d
EelNoodles <as06180618@gmail.com>
2023-06-08 22:32:16 +0800

    Update cloud-app-info.js

--- cloud-app-info.js
+++ cloud-app-info.js
@@ -1,10 +1,10 @@
 ﻿
 var AppInfo = {
-//	Wss: true,
+	Wss: true,
     AppId: "ec4417df-e2eb-4b9c-b985-e8a1ed6cda95",
     AppVersion: "1.0",
-    MasterServer: "wss://GCAMS1015.exitgames.com:80",
-    NameServer: "wss://ns.photonengine.io:19093"
+//  MasterServer: "wss://GCAMS1015.exitgames.com:80",
+//  NameServer: "wss://ns.photonengine.io:19093"
 //  FbAppId: "you fb app id", 
 }
 
