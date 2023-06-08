6cda703dd0b985aebf795b3260b59c2c26b27e04
EelNoodles <as06180618@gmail.com>
2023-06-08 22:22:04 +0800

    Update cloud-app-info.js

--- cloud-app-info.js
+++ cloud-app-info.js
@@ -3,7 +3,7 @@ var AppInfo = {
 //	Wss: true,
     AppId: "ec4417df-e2eb-4b9c-b985-e8a1ed6cda95",
     AppVersion: "1.0",
-//	MasterServer: "localhost:9090"
+	MasterServer: "https://chemistrylms.onrender.com/"
     NameServer: "https://chemistrylms.onrender.com/"
 //  FbAppId: "you fb app id", 
 }
